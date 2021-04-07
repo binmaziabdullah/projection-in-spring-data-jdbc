e320a36b310a6d4f46fbd6a502bd521d7cc340da
abdullah <binmazi22@gmail.com>
2021-04-07 13:00:43 +0530

    did some improvement in sql queries (using clause is added in inner join query)

--- src/main/java/projectionusingspringdatajdbc/projection/in/spring/data/jdbc/StudentRepository.java
+++ src/main/java/projectionusingspringdatajdbc/projection/in/spring/data/jdbc/StudentRepository.java
@@ -12,9 +12,14 @@ public interface StudentRepository extends CrudRepository<Student,Long> {
     @Query(value = "select s.name as name , s.id as id from student s")
     List<StudentProjection> getStudentByNameANId();
 
-    @Query(value = "select s.id as id, s.name as name , " +
-            "a.city as city ,a.state as state,a.country as country " +
-            "from student s " +
-            "inner join address a on s.add_id = a.add_id")
+    /*here we are using inner join to linked or join two table
+    i.e student and address using some predicate (common data/column) in our case
+    it is add_id column which is same in both the tables and also add_id is a foreign key.
+    in student table.
+
+    and we can use "using" clause if we have same column in both the
+    table(we have same table add_id in both the table)*/
+
+    @Query(value = "select s.id as id, s.name as name , a.city as city, a.state as state, a.country as country from Student s inner join address a using(add_id)")
     List<StudentProjection> getStudentAndAddress();
 }
